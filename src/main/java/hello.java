020c77cad756b14cacfb033b5144b4772b371c56
yyy <yyy@yyy.com>
2020-03-06 18:13:26 +0800

    添加

---
+++ src/main/java/hello.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("git/...........");
+        System.out.println("git/...........");
+        System.out.println("git/...........");
+        System.out.println("git/...........");
+    }
+}
